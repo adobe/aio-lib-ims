bb96a4a80b9d36ba55341eb42254dd004f107a50
Felix Meschberger <fmeschbe@adobe.com>
2019-05-29 20:08:35 -0700

    Initial commit of an IMS API library

---
+++ src/ims.js
@@ -0,0 +1,216 @@
+/*
+Copyright 2018 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const rp = require('request-promise-native')
+const debug = require('debug')('@adobe/aio-cli-ims');
+
+const IMS_ENDPOINTS = {
+    stage: "https://ims-na1-stg1.adobelogin.com",
+    prod: "https://ims-na1.adobelogin.com"
+}
+
+const ACCESS_TOKEN = "access_token";
+const REFRESH_TOKEN = "refresh_token";
+const AUTHORIZATION_CODE = "authorization_code";
+const CLIENT_ID = "client_id";
+const CLIENT_SECRET = "client_secret";
+const SCOPE = "scope";
+
+async function sendPost(postUrl, postData) {
+
+    const options = {
+        uri: postUrl,
+        method: 'POST',
+        form: postData,
+        json: true
+    }
+
+    return rp(options);
+}
+
+function _getTokenPayloadJson(token) {
+    const [header, payload, sig] = token.split(".", 3);
+    return JSON.parse(Buffer.from(payload, 'base64'));
+}
+
+function calculateExpiry(token) {
+    // Note: could use jwt library, but this is simpler and slicker
+    const data = _getTokenPayloadJson(token);
+    return parseInt(data.created_at) + parseInt(data.expires_in);
+}
+
+function getTokenType(token) {
+    return _getTokenPayloadJson(token).type;
+}
+
+/**
+ * ```json
+ * {
+ *  "access_token": {
+ *    "token": <access_token>,
+ *    "expiry": <expiry-time-in-ms-since-epoch>
+ *  },
+ *  "refresh_token": {
+ *    "token": >refresh_token>,
+ *    "expiry": <expiry-time-in-ms-since-epoch>
+ *  },
+ *  "payload": {
+ *   <full-response>
+ *  }
+ * }
+ * ```
+ */
+async function toTokenResult(apiResponse) {
+    debug("toTokenResult(%o)", apiResponse);
+    const result = {
+        payload: apiResponse
+    }
+
+    for (const label of [ ACCESS_TOKEN, REFRESH_TOKEN ]) {
+        debug(" > %s", label);
+        const token = apiResponse[label];
+        debug(" > %o", token);
+        if (token) {
+            result[label] = {
+                token: token,
+                expiry: calculateExpiry(token)
+            }
+            debug(" > %o", result[label]);
+        }
+    }
+
+    debug("<< %o", result);
+    return result;
+}
+
+class Ims {
+
+    /**
+     * Creats a new IMS connector instance for the stage or prod environment
+     * @param {string} env
+     */
+    constructor(env) {
+        if (!env || !IMS_ENDPOINTS[env]) {
+            env = "stage";
+        }
+
+        this.endpoint = IMS_ENDPOINTS[env];
+    }
+
+    getApiUrl(api) {
+        return this.endpoint + api;
+    }
+
+    getSusiUrl(clientId, scopes, callbackUrl, state) {
+        debug("getSusiUrl(%s, %s, %s, %s)", clientId, scopes, callbackUrl, state);
+
+        const app = new URL(this.getApiUrl("/ims/authorize/v1"));
+        app.searchParams.set("response_type", "code");
+        app.searchParams.set(CLIENT_ID, clientId);
+        app.searchParams.set(SCOPE, scopes);
+        app.searchParams.set("redirect_uri", callbackUrl);
+        app.searchParams.set("state", state);
+        return app.toString();
+    }
+
+    /**
+     *
+     * @param {string} authCode
+     * @param {string} clientId
+     * @param {string} clientSecret
+     * @param {string} scopes
+     *
+     * @returns a promise resolving to a tokens object as described in the {@link toTokenResult} or rejects to an error message.
+     */
+    async getAccessToken(authCode, clientId, clientSecret, scopes) {
+        debug("getAccessToken(%s, %s, %s, %o)", authCode, clientId, clientSecret, scopes);
+
+        // prepare the data with common data
+        const postData = {
+            client_id: clientId,
+            client_secret: clientSecret,
+            scope: scopes
+        }
+
+        // complete data with authCode specific grant type and property
+        const tokenType = getTokenType(authCode);
+        if (tokenType == AUTHORIZATION_CODE) {
+            // for service tokens this is the static authCode
+            // for OAuth Tokerns this is the code received from the redirect
+            postData.grant_type = AUTHORIZATION_CODE;
+            postData.code = authCode;
+        } else if (tokenType == REFRESH_TOKEN) {
+            // for refresh tokens
+            postData.grant_type = REFRESH_TOKEN;
+            postData.refresh_token = authCode;
+        } else {
+            Promise.reject(`Unknown type of authCode: ${tokenType}`);
+        }
+
+        return sendPost(this.getApiUrl("/ims/token/v1"), postData)
+            .then(response => toTokenResult(response));
+    }
+
+    /**
+     * Asks for the signed JWT token to be exchanged for a valid access
+     * token as well as a refresh token.
+     *
+     * @param {string} clientId The client ID of the owning application
+     * @param {string} clientSecret The client's secret
+     * @param {string} signedJwtToken The properly signed JWT token for the JWT token exchange
+     */
+    async exchangeJwtToken(clientId, clientSecret, signedJwtToken) {
+        debug("exchangeJwtToken(%s, %s, %s)", clientId, clientSecret, signedJwtToken);
+
+        const postData = {
+            client_id: clientId,
+            client_secret: clientSecret,
+            jwt_token: signedJwtToken
+        }
+
+        return sendPost(this.getApiUrl("/ims/exchange/jwt"), postData)
+            .then(response => toTokenResult(response));
+    }
+
+    /**
+     * Invalidates the given token. If the token is a refresh token, all the
+     * access tokens created with that refresh token will also be invalidated
+     * at the same time.
+     *
+     * @param {string} token
+     * @param {string} clientId
+     * @param {string} clientSecret
+     */
+    async invalidateToken(token, clientId, clientSecret) {
+        debug("invalidateToken(%s, %s, %s)", token, clientId, clientSecret);
+
+        const postData = {
+            token_type: getTokenType(token),
+            token,
+            cascading: "all",
+            client_id: clientId,
+            client_secret: clientSecret
+        };
+
+        return sendPost(this.getApiUrl("/ims/invalidate_token/v2"), postData);
+    }
+}
+
+module.exports = {
+    Ims,
+    ACCESS_TOKEN,
+    REFRESH_TOKEN,
+    AUTHORIZATION_CODE,
+    CLIENT_ID,
+    CLIENT_SECRET,
+    SCOPE
+};
