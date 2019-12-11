7f3b34b2298b88fbd26f114cc2d46e6a1329753d
Shazron Abdullah (shaz) <shazron@gmail.com>
2019-12-11 11:56:55 +0800

    task: cleaned up tests

--- test/token-helper.test.js
+++ test/token-helper.test.js
@@ -17,6 +17,29 @@ afterEach(() => {
   jest.restoreAllMocks()
 })
 
+function createHandlerForContext (context = {}, imsPlugin) {
+  const mappedContext = Object.keys(context)
+    // prefix $ims. to all the keys
+    .map(key => {
+      return {
+        [`$ims.${key}`]: context[key]
+      }
+    })
+    // merge the objects
+    .reduce((acc, cur) => {
+      return Object.assign(acc, cur)
+    }, {})
+
+  const store = {
+    ...mappedContext,
+    '$ims.$plugins': imsPlugin ? [imsPlugin] : null
+  }
+
+  return function (key) {
+    return store[key]
+  }
+}
+
 test('exports', async () => {
   expect(typeof IMS_TOKEN_MANAGER).toEqual('object')
 })
@@ -34,170 +57,185 @@ test('getTokenIfValid', async () => {
 })
 
 test('getToken - string', async () => {
-  config.get.mockImplementation(key => {
-    if (key === '$ims.known-context') {
-      return {
-        foo: 'bar',
-        returnType: 'string',
-        expired: 'no'
-      }
-    } else if (key === '$ims.$plugins') {
-      return ['../test/ims-plugins/imsPlugin']
+  const contextName = 'known-context'
+  const context = {
+    [contextName]: {
+      foo: 'bar',
+      returnType: 'string',
+      expired: 'no'
     }
-  })
-
-  // // unknown context
-  let contextName = 'unknown-context'
-  await expect(IMS_TOKEN_MANAGER.getToken(contextName, false)).rejects.toEqual(new Error(`IMS context '${contextName}' is not configured`))
+  }
 
   // known context, handled by ./test/ims-plugins/imsPlugin
-  contextName = 'known-context'
+  config.get.mockImplementation(
+    createHandlerForContext(context, '../test/ims-plugins/imsPlugin')
+  )
+
+  // no force
   await expect(IMS_TOKEN_MANAGER.getToken(contextName, false)).resolves.toEqual('abc123')
 
-  // known context, handled by ./test/ims-plugins/imsPlugin (force)
-  contextName = 'known-context'
+  // force
   await expect(IMS_TOKEN_MANAGER.getToken(contextName, true)).resolves.toEqual('abc123')
 })
 
 test('getToken - object', async () => {
-  config.get.mockImplementation(key => {
-    if (key === '$ims.known-context') {
-      return {
-        foo: 'bar',
-        returnType: 'object',
-        expired: 'no',
-        refresh_token: {
-          token: 'abcd123',
-          expiry: Date.now() + 20 * 60 * 1000 // 20 minutes from now
-        }
-      }
-    } else if (key === '$ims.$plugins') {
-      return ['../test/ims-plugins/imsPlugin']
-    }
-  })
-
-  // known context, handled by ./test/ims-plugins/imsPlugin (force)
   const contextName = 'known-context'
+  const context = {
+    [contextName]: {
+      foo: 'bar',
+      returnType: 'object',
+      expired: 'no',
+      refresh_token: {
+        token: 'abcd123',
+        expiry: Date.now() + 20 * 60 * 1000 // 20 minutes from now
+      }
+    }
+  }
+
+  // known context, handled by ./test/ims-plugins/imsPlugin
+  config.get.mockImplementation(
+    createHandlerForContext(context, '../test/ims-plugins/imsPlugin')
+  )
+
+  // no force
   await expect(IMS_TOKEN_MANAGER.getToken(contextName, false)).resolves.toEqual('abc123')
-})
-
-test('getToken - object (refresh token expired)', async () => {
-  config.get.mockImplementation(key => {
-    if (key === '$ims.known-context') {
-      return {
-        foo: 'bar',
-        returnType: 'object',
-        expired: 'refresh_token'
-      }
-    } else if (key === '$ims.$plugins') {
-      return ['../test/ims-plugins/imsPlugin']
-    }
-  })
-
-  // known context, handled by ./test/ims-plugins/imsPlugin (force)
-  const contextName = 'known-context'
+  // force
   await expect(IMS_TOKEN_MANAGER.getToken(contextName, true)).resolves.toEqual('abc123')
 })
 
-test('invalidateToken', async () => {
-  config.get.mockImplementation(key => {
-    if (key === '$ims.known-context') {
-      return {
-        foo: 'bar',
-        returnType: 'string',
-        expired: 'no',
-        access_token: {
-          token: 'abcd123',
-          expiry: Date.now() + 20 * 60 * 1000 // 20 minutes from now
-        }
-      }
-    } else if (key === '$ims.$plugins') {
-      return ['../test/ims-plugins/imsPlugin']
+test('getToken - object (refresh token expired)', async () => {
+  const contextName = 'known-context'
+  const context = {
+    [contextName]: {
+      foo: 'bar',
+      returnType: 'object',
+      expired: 'refresh_token'
     }
-  })
-
-  // unknown context
-  let contextName = 'unknown-context'
-  await expect(IMS_TOKEN_MANAGER.invalidateToken(contextName, false)).rejects.toEqual(new Error(`IMS context '${contextName}' is not configured`))
+  }
 
   // known context, handled by ./test/ims-plugins/imsPlugin
-  contextName = 'known-context'
+  config.get.mockImplementation(
+    createHandlerForContext(context, '../test/ims-plugins/imsPlugin')
+  )
+
+  // no force
+  await expect(IMS_TOKEN_MANAGER.getToken(contextName, false)).resolves.toEqual('abc123')
+  // force
+  await expect(IMS_TOKEN_MANAGER.getToken(contextName, true)).resolves.toEqual('abc123')
+})
+
+test('invalidateToken - has access and refresh token', async () => {
+  const contextName = 'known-context'
+  const context = {
+    [contextName]: {
+      foo: 'bar',
+      returnType: 'object',
+      expired: 'no',
+      access_token: {
+        token: 'abcd123',
+        expiry: Date.now() + 20 * 60 * 1000 // 20 minutes from now
+      },
+      refresh_token: {
+        token: 'wxyz123',
+        expiry: Date.now() + 20 * 60 * 1000 // 20 minutes from now
+      }
+    }
+  }
+
+  // known context, handled by ./test/ims-plugins/imsPlugin
+  config.get.mockImplementation(
+    createHandlerForContext(context, '../test/ims-plugins/imsPlugin')
+  )
+
+  // no force
   await expect(IMS_TOKEN_MANAGER.invalidateToken(contextName, false)).resolves.not.toThrow()
 
-  // known context, handled by ./test/ims-plugins/imsPlugin (force)
-  contextName = 'known-context'
+  // force
   await expect(IMS_TOKEN_MANAGER.invalidateToken(contextName, true)).resolves.not.toThrow()
 })
 
-test('invalidateToken - expired', async () => {
-  config.get.mockImplementation(key => {
-    if (key === '$ims.known-context') {
-      return {
-        foo: 'bar',
-        returnType: 'string',
-        expired: 'no',
-        access_token: {
-          token: 'abcd123',
-          expiry: Date.now() - 5000 // in the past
-        }
-      }
-    } else if (key === '$ims.$plugins') {
-      return ['../test/ims-plugins/imsPlugin']
-    }
-  })
+test('invalidateToken - unknown context', async () => {
+  const contextName = 'unknown-context'
+  const context = {}
 
-  // known context, handled by ./test/ims-plugins/imsPlugin (no force)
+  // unknown context
+  config.get.mockImplementation(
+    createHandlerForContext(context)
+  )
+
+  // no force
+  await expect(IMS_TOKEN_MANAGER.invalidateToken(contextName, false)).rejects.toEqual(new Error(`IMS context '${contextName}' is not configured`))
+  // force
+  await expect(IMS_TOKEN_MANAGER.invalidateToken(contextName, false)).rejects.toEqual(new Error(`IMS context '${contextName}' is not configured`))
+})
+
+test('invalidateToken - token missing or expired', async () => {
   const contextName = 'known-context'
+  const context = {
+    [contextName]: {
+      foo: 'bar',
+      returnType: 'object'
+    }
+  }
+
+  // known context, handled by ./test/ims-plugins/imsPlugin
+  config.get.mockImplementation(
+    createHandlerForContext(context, '../test/ims-plugins/imsPlugin')
+  )
+
+  // no force
   await expect(IMS_TOKEN_MANAGER.invalidateToken(contextName, false)).rejects.toEqual(new Error('Token missing or expired'))
+  // force
+  await expect(IMS_TOKEN_MANAGER.invalidateToken(contextName, true)).rejects.toEqual(new Error('Token missing or expired'))
 })
 
 test('getToken - unknown plugin', async () => {
-  config.get.mockImplementation(key => {
-    if (key === '$ims.known-context') {
-      return {
-        foo: 'bar'
-      }
-    } else if (key === '$ims.$plugins') {
-      return null
+  const contextName = 'known-context'
+  const context = {
+    [contextName]: {
+      foo: 'bar'
     }
-  })
+  }
 
   // plugin not specified
-  const contextName = 'known-context'
+  config.get.mockImplementation(
+    createHandlerForContext(context)
+  )
+
   await expect(IMS_TOKEN_MANAGER.getToken(contextName, false))
     .rejects.toEqual(new Error('Cannot generate token because no plugin supports configuration'))
 })
 
-test('getToken - bad plugin', async () => {
-  config.get.mockImplementation(key => {
-    if (key === '$ims.known-context') {
-      return {
-        bar: 'foo'
-      }
-    } else if (key === '$ims.$plugins') {
-      return ['../test/ims-plugins/badImsPlugin']
-    }
-  })
-
-  // coverage: bad plugin, supports function throws an exception
+test('getToken - bad ims plugin, throws exception (coverage)', async () => {
   const contextName = 'known-context'
+  const context = {
+    [contextName]: {
+      bar: 'foo'
+    }
+  }
+
+  config.get.mockImplementation(
+    createHandlerForContext(context, '../test/ims-plugins/badImsPlugin')
+  )
+
+  // `supports` function throws an exception
   await expect(IMS_TOKEN_MANAGER.getToken(contextName, false))
     .rejects.toEqual(new Error('Cannot generate token because no plugin supports configuration'))
 })
 
-test('getToken - incomplete plugin', async () => {
-  config.get.mockImplementation(key => {
-    if (key === '$ims.known-context') {
-      return {
-        somekey: 'xyz'
-      }
-    } else if (key === '$ims.$plugins') {
-      return ['../test/ims-plugins/incompleteImsPlugin']
-    }
-  })
-
-  // coverage: incomplete plugin
+test('getToken - incomplete ims plugin (coverage)', async () => {
   const contextName = 'known-context'
+  const context = {
+    [contextName]: {
+      somekey: 'xyz'
+    }
+  }
+
+  config.get.mockImplementation(
+    createHandlerForContext(context, '../test/ims-plugins/incompleteImsPlugin')
+  )
+
+  // imsLogin function is missing
   await expect(IMS_TOKEN_MANAGER.getToken(contextName, false))
     .rejects.toEqual(new Error('Cannot generate token because no plugin supports configuration'))
 })
