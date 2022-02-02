97525b655be2196289f9d8f2c2f2456b7ec683b1
Jesse MacFadyen <purplecabbage@gmail.com>
2022-02-01 16:28:48 -0800

    form-data is a dependency, not just dev

--- src/ims.js
+++ src/ims.js
@@ -9,7 +9,7 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
-// eslint-disable-next-line node/no-unpublished-require
+
 const FormData = require('form-data')
 const { HttpExponentialBackoff } = require('@adobe/aio-lib-core-networking')
 const aioLogger = require('@adobe/aio-lib-core-logging')('@adobe/aio-lib-ims:ims', { provider: 'debug' })
